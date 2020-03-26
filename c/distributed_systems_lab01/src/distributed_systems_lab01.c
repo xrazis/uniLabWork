575f63f6bfebc7414d7b09f1e70ca2fde20c017c
xrazis <xaris.ws98@gmail.com>
2020-03-26 23:01:27 +0200

    Init

---
+++ c/distributed_systems_lab01/src/distributed_systems_lab01.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : distributed_systems_lab01.c
+ Author      : Haris Razis - cs161072
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
