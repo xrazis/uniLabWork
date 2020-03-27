01e6d783e625c9f823c879b51245e99bb773c1c7
xrazis <xaris.ws98@gmail.com>
2020-03-27 16:21:05 +0200

    Init server / client eclipse projects

---
+++ c/distributed_systems_lab01/client/src/client.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : client.c
+ Author      : Haris Razis - cs161072
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello client!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
--- c/distributed_systems_lab01/server/src/server.c
+++ c/distributed_systems_lab01/server/src/server.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : distributed_systems_lab01.c
+ Name        : server.c
  Author      : Haris Razis - cs161072
  Version     :
  Copyright   : Your copyright notice
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello server!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
