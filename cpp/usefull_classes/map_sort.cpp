28b770cbf6c1fa08b1c3b34f05b314072db630f1
Haris Razis <dev@xrazis.com>
2021-02-08 09:00:05 +0200

    Delete cpp directory

--- cpp/usefull_classes/map_sort.cpp
+++
@@ -1,7 +0,0 @@
-//But wait, there is a better approach: store std::pair<V,K> instead of std::pair<K,V>
-// and then you don't need any comparer at all — the standard comparer for std::pair
-// would be enough, as it compares first (which is V) first then second which is K:
-
-std::vector<std::pair<V,K>> items;
-//...
-std::sort(items.begin(), items.end());
